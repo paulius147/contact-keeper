dacd6f1f14af3d23f66ad20f8e59bfe3259cfc16
Paulius147 <pauliusp147@gmail.com>
2021-11-20 23:15:24 +0200

    deploy

--- config/db.js
+++ config/db.js
@@ -6,15 +6,16 @@ const connectDB = async () => {
     try {
         await mongoose.connect(db, {
             useNewUrlParser: true,
-            // useCreateIndex: true,
-            // useFindAndModify: false,
-            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
         });
-        console.log('MongoDB Connected');
+
+        console.log('MongoDB Connected...');
     } catch (err) {
         console.error(err.message);
         process.exit(1);
     }
-}
+};
 
 module.exports = connectDB;
