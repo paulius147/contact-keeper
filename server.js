271fb630239f3dc800143af77289ea21c58e4af3
Paulius147 <pauliusp147@gmail.com>
2021-11-20 22:47:14 +0200

    deploy

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
@@ -10,19 +11,19 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// Define routes
+// Define Routes
 app.use('/api/users', require('./routes/users'));
-app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
-// Server static assets in production
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-    // set static folder
+    // Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
 }
 
 const PORT = process.env.PORT || 5000;
